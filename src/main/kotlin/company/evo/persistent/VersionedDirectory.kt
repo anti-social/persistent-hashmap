e336127be6c03220e4270d4a264c14e37ff0a66a
Alexander Koval <kovalidis@gmail.com>
2019-03-29 16:55:48 +0200

    Basic documentation for VersionedDirectory interface

--- src/main/kotlin/company/evo/persistent/VersionedDirectory.kt
+++ src/main/kotlin/company/evo/persistent/VersionedDirectory.kt
@@ -5,7 +5,6 @@ import company.evo.io.MutableIOBuffer
 import company.evo.rc.RefCounted
 
 import java.io.Closeable
-import java.nio.ByteOrder
 import java.nio.file.Path
 
 open class VersionedDirectoryException(
@@ -19,21 +18,51 @@ class ReadOnlyException(msg: String) : VersionedDirectoryException(msg)
 class FileAlreadyExistsException(path: Path) : VersionedDirectoryException("Cannot create $path: already exists")
 class FileDoesNotExistException(path: Path) : VersionedDirectoryException("Cannot open $path: does not exist")
 
-// class MappedFile(
-//         val buffer:
-// )
-
 interface VersionedDirectory : Closeable {
+    /**
+     * Reads a version from the version file.
+     */
     fun readVersion(): Long
+
+    /**
+     * Writes a [version] to the version file.
+     */
     fun writeVersion(version: Long)
+
+    /**
+     * Creates a file with [name] and [size] and maps it into a buffer.
+     * @return a [MutableIOBuffer] wrapped into [RefCounted].
+     *
+     * Ownership of the returned value is moved to the caller of the method.
+     * So it is a user responsibility to close the file.
+     */
     fun createFile(name: String, size: Int): RefCounted<MutableIOBuffer>
+
+    /**
+     * Opens an existing file with [name] and maps it into a buffer.
+     * @return a [MutableIOBuffer] wrapped into [RefCounted].
+     *
+     * Ownership of the returned value is moved to the caller of the method.
+     * So it is a user responsibility to close the file.
+     */
     fun openFileWritable(name: String): RefCounted<MutableIOBuffer>
+
+    /**
+     * Opens an existing file with [name] in read-only mode and maps it into a buffer.
+     * @return a [MutableIOBuffer] wrapped into [RefCounted].
+     *
+     * Ownership of the returned value is moved to the caller of the method.
+     * So it is a user responsibility to close the file.
+     */
     fun openFileReadOnly(name: String): RefCounted<IOBuffer>
+
+    /**
+     * Deletes a file with [name].
+     */
     fun deleteFile(name: String)
 
     companion object {
         const val VERSION_LENGTH = 8
-        // val BYTE_ORDER: ByteOrder = ByteOrder.LITTLE_ENDIAN
     }
 }
 
