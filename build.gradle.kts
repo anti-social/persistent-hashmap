976efd84454f6bf8ea9941169d5fe09959278d17
Alexander Koval <kovalidis@gmail.com>
2025-10-20 12:31:51 +0300

    Remove explicit kotlin dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,8 +42,6 @@ dependencies {
     val kotestVersion = "4.6.0"
     val lincheckVersion = "2.14.1"
 
-    implementation(kotlin("stdlib-jdk8"))
-    implementation(kotlin("reflect"))
     implementation("org.slf4j", "slf4j-api", slf4jVersion)
 
     compileOnly(project(":processor"))
